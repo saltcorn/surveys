7b1117ccd46b93695ebe3d33c18b7638533026cd
Tom Nielsen <tanielsen@gmail.com>
2024-06-12 12:00:41 +0100

    wip question_config

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
-const { select, option, text_attr } = require("@saltcorn/markup/tags");
+const {
+  select,
+  option,
+  text_attr,
+  input,
+  script,
+  domReady,
+  div,
+} = require("@saltcorn/markup/tags");
+const Table = require("@saltcorn/data/models/table");
 
 const questionType = {
   name: "Question type",
@@ -48,12 +57,55 @@ const questionType = {
   },
 };
 
-const dependencies = ["@saltcorn/tinymce"];
+const question_configuration = {
+  type: "JSON",
+  isEdit: true,
+  blockDisplay: true,
+  handlesTextStyle: true,
+  configFields: async (field) => {
+    const table = Table.findOne(field.table_id);
+    const fields = table.fields;
+    return [
+      {
+        name: "type_field",
+        label: "Question type field",
+        type: "String",
+        required: true,
+        attributes: {
+          options: fields
+            .filter((f) => f.type?.name === "Question type")
+            .map((f) => f.name),
+        },
+      },
+    ];
+  },
+  run: (nm, v, attrs, cls) => {
+    const rndcls = `tmce${Math.floor(Math.random() * 16777215).toString(16)}`;
+    return div(
+      { id: `qconfig${rndcls}` },
+      input({ type: "hidden" }),
+      script(
+        domReady(`
+    function update_qconfig_${rndcls}() {
+       const qtype=get_form_record($("#qconfig${rndcls}")).${attrs.type_field}
+       console.log("qtype",qtype);
+    }
+    update_qconfig_${rndcls}();
+    $("#qconfig${rndcls}").closest('form[data-viewname]').on('change', update_qconfig_${rndcls})    
+      `)
+      )
+    );
+  },
+};
+
+const dependencies = ["@saltcorn/tinymce", "@saltcorn/json"];
 
 module.exports = {
   sc_plugin_api_version: 1,
   plugin_name: "surveys",
   viewtemplates: [require("./runsurvey")],
   types: [questionType],
+  fieldviews: { question_configuration },
+
   dependencies,
 };
